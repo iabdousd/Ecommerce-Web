8fe9c38bd50065dede3369ef36043e1566070bec
iabdousd <abdulrahmansidimed@gmail.com>
2019-11-16 13:33:08 +0000

    Fixed mobile login page

--- login/login.js
+++ login/login.js
@@ -1,5 +1,8 @@
 function mobile_login() {
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        document.open('login-mb.html')
+    var isMobile = window.matchMedia("only screen and (max-width: 400px)").matches;
+    if (isMobile){
+        window.open('login-mb.html', "_self");
+    }else{
+        alert('You are in a desktop device don\'t be fool.');
     }
 }
